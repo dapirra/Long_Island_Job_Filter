2348999bfb581ef73e561c0ad24b1f45d2cdbd69
dapirra <35818038+dapirra@users.noreply.github.com>
2021-03-26 06:03:55 -0400

    del: jquery dependency

--- Long Island Job FIlter.user.js
+++ Long Island Job FIlter.user.js
@@ -4,13 +4,11 @@
 // @version      0.1
 // @description  Filters out jobs that are not on Long Island from the search results of popular job search engines.
 // @match        http*://www.indeed.com/jobs?*
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
 // ==/UserScript==
 
 /*
 	Libraries:
-	* jQuery: https://jquery.com/
 	* arrive.js: https://github.com/uzairfarooq/arrive/
 */
 
